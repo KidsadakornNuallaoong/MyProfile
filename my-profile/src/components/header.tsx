46f994e18bfb4e25a7936ead8a8672fde949a8a5
Kidsadakorn Nuallaoong <121489701+KidsadakornNuallaoong@users.noreply.github.com>
2024-05-01 18:39:32 +0700

    Update Responsive
    Responsive at home page

--- my-profile/src/components/header.tsx
+++ my-profile/src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div className="w-screen flex items-center justify-around">
           <img src={Linker.Logo.white.path} alt={Linker.Logo.white.component} width={50} />
             <nav className=''>
-              <ul className='hidden sm:justify-center sm:text-[15px] md:text-[17px] md:flex md:space-x-[7vw] lg:space-x-[7vw] '>
+              <ul className='hidden sm:justify-center sm:text-[15px] md:text-[17px] md:flex md:space-x-[5vw] font-bold'>
                 <li><a className='hover:underline hover:text-quaternary' href="/">Home</a></li>
                 <li><a className='hover:underline hover:text-quaternary' href="/profile">Profile</a></li>
                 <li><a className='hover:underline hover:text-quaternary' href="/project">Project</a></li>
